9121ddc75a3921c7c73dbb9879981a37dfa4a045
stuphys1729 <s1432366@sms.ed.ac.uk>
2019-01-25 22:15:10 +0000

    Added to solr test and webapp functionality to use pre-scraped memes

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -14,7 +14,7 @@ def search():
 
     return render_template('index.html', results=link_results, search_term=search_term)
 
-@app.route('/meme/<int:meme_id>')
+@app.route('/meme/<string:meme_id>')
 def meme(meme_id):
     meme_result = test_results()[meme_id]
     return render_template('image.html', result=meme_result)
