4810e155a4f436ef98c0e2028d104aede4760386
stuphys1729 <s1432366@sms.ed.ac.uk>
2019-02-01 11:39:43 +0000

    Small changes to code format and gitignore

--- src/webapp/scraping.py
+++ src/webapp/scraping.py
@@ -9,9 +9,12 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\t
 import numpy as np
 
 subreddit = 'dankmemes'
-# Connect to reddit and download the subreddit front page
-_secret     = 'agAYm70wPBewuKbnCAnAWLiH0Zk'# You will need to get your own credentials to put here
-_clientId   = 'GqTsEKnK6fI-rw'# and here, see link above.
+
+# The credentials below are linked to my (Stewart) reddit account, if you want
+# to do any of your own research please generate your own (guide linked to from
+# my RedditScrape.ipynb file)
+_secret     = 'agAYm70wPBewuKbnCAnAWLiH0Zk'
+_clientId   = 'GqTsEKnK6fI-rw'
 _user_agent = 'windows:FindYourMeme:0.1'
 r = praw.Reddit(client_id=_clientId, client_secret=_secret, user_agent=_user_agent)
 image_extensions = ('.jpg', '.png', '.gif')
@@ -37,7 +40,8 @@ def update_meme_data(memeData):
             "title": sub.title,
             "url"  : sub.url,
             "plink": sub.permalink,
-            "time" : sub.created_utc
+            "time" : sub.created_utc,
+            "sub"  : subreddit
         }
 
         im = Image.open(requests.get(sub.url, stream=True).raw)
