5d8117b96644a910fbbe382296a46ca0a825dd06
stuphys1729 <s1432366@sms.ed.ac.uk>
2019-02-05 22:24:13 +0000

    Added a bunch more meme formats to the spider

--- dankKnowledge/dankKnowledge/spiders/KYMSpider.py
+++ dankKnowledge/dankKnowledge/spiders/KYMSpider.py
@@ -1,13 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-formats = ['DrakePosting', 'scumbag-steve', 'good-guy-greg', 'bad-luck-brian', 'pepe-the-frog']
-thisRun = 4
+formats = [
+    'DrakePosting',# 0
+    'scumbag-steve',
+    'good-guy-greg',
+    'bad-luck-brian',
+    'pepe-the-frog',
+    'but-thats-none-of-my-business',# 5
+    'everyone-loses-their-minds',
+    '10-guy',
+    'actual-advice-mallard',
+    'confession-bear',
+    'sudden-clarity-clarence',
+    'awkward-moment-seal',
+    'seal-of-approval',
+    'minor-mistake-marvin',
+    'the-rent-is-too-damn-high-jimmy-mcmillan',
+    'afraid-to-ask-andy',
+    'first-world-problems',
+    'pepperidge-farm-remembers',
+    'you-know-what-really-grinds-my-gears',
+    'matrix-morpheus',
+    'that-would-be-great',
+    'daily-struggle',
+    'kevin-durant-mvp-speech',
+    'is-this-a-pigeon',
+    'annoyed-picard',
+    'do-you-want-ants',
+    'ultra-instinct-shaggy'
+    ]
+#thisRun = 9
 
 class KYMSpider(scrapy.Spider):
     name = "KYMSpider"
     allowed_domains = ['knowyourmeme.com']
-    start_urls = ['https://knowyourmeme.com/memes/{}/photos'.format(formats[thisRun])]
+    start_urls = ['https://knowyourmeme.com/memes/{}/photos'.format(formats[-1])]
     base = 'https://knowyourmeme.com'
 
     def parse(self, response):
@@ -22,9 +50,9 @@ class KYMSpider(scrapy.Spider):
 
     def parseImLink(self, response):
 
-            imLink = response.xpath('//div[@id="photo_wrapper"]').xpath('a/@href').extract_first()
-            scraped_imlinks = {
-                'image_urls' : [imLink],
-            }
+        imLink = response.xpath('//div[@id="photo_wrapper"]').xpath('a/@href').extract_first()
+        scraped_imlinks = {
+            'image_urls' : [imLink],
+        }
 
-            yield scraped_imlinks
+        yield scraped_imlinks
