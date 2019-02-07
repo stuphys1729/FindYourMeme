8046db98ba7ec773ddb68653f8c4ca9f502d5503
philMarius <philipcumner@gmail.com>
2019-02-07 14:54:29 +0000

    Start playing with sqlite datetime functions

--- src/webapp/test_datetime.py
+++ src/webapp/test_datetime.py
@@ -1,7 +1,17 @@
 import sqlite3
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 conn_m = sqlite3.connect('memes.db')
 conn_n = sqlite3.connect('new_memes.db')
+
+conn_m.row_factory = dict_factory
+conn_n.row_factory = dict_factory
 
 cm = conn_m.cursor()
 cn = conn_n.cursor()
@@ -20,4 +30,10 @@ cn.execute('''
                 , upvote_ratio REAL
                 , over_18 TEXT
                 , time_of_index DATETIME
+                , format TEXT
         )''')
+conn_n.commit()
+
+for record in cm.execute("SELECT * FROM memes").fetchall():
+    # TODO get the datetime field and add to db as unixepoch
+    print(record)
