e2996fbcbcbf91f3ab13e3b0d394fd7319274c27
philMarius <philipcumner@gmail.com>
2019-02-07 18:31:23 +0000

    Add migrations for adding correctly formatted datetime

--- src/webapp/migration_add_time_to_db.py
+++ src/webapp/migration_add_time_to_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime as dt
 
 def dict_factory(cursor, row):
     d = {}
@@ -6,18 +7,19 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-
 conn_m = sqlite3.connect('memes.db')
 conn_n = sqlite3.connect('new_memes.db')
 
 conn_m.row_factory = dict_factory
-conn_n.row_factory = dict_factory
 
 cm = conn_m.cursor()
 cn = conn_n.cursor()
 
+# cn.execute('''DROP TABLE memes''')
+# conn_n.commit()
+
 cn.execute('''
-            CREATE TABLE IF NOT EXISTS memes(
+            CREATE TABLE memes(
                 id PRIMARY KEY
                 , title TEXT
                 , url TEXT
@@ -34,6 +36,15 @@ cn.execute('''
         )''')
 conn_n.commit()
 
-for record in cm.execute("SELECT * FROM memes").fetchall():
-    # TODO get the datetime field and add to db as unixepoch
-    print(record)
+results = cm.execute("SELECT * FROM memes").fetchall()
+
+for record in results:
+    record.update((k, dt.utcfromtimestamp(float(v)).strftime('%Y-%m-%d %H:%M:%S')) for k, v in record.items() if k == "time")
+
+results_tuples = [tuple(d.values()) for d in results]
+print(len(results_tuples[0]))
+
+cn.executemany('''INSERT OR REPLACE INTO memes
+    VALUES(?,?,?,?,datetime(?),?,?,?,?,?,?,datetime(?),?)''',
+    results_tuples)
+conn_n.commit()
